ce40ff8c3bc1e29e14b412f312f5d09d3b521ade
yash9943 <yr14112004@gmail.com>
2025-10-01 17:03:21 +0530

    Sheet3

--- Sheet3/problem10.py
+++ Sheet3/problem10.py
@@ -2,3 +2,20 @@
  Q10. Decorator for Function Execution Time (10 Marks)
  Write a decorator that measures how long a function takes to execute.
 '''
+import time
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Exectution time for {func.__name__} : {execution_time:.8f} seconds")
+        return result,
+    return wrapper
+
+@timeit
+def printit():
+    print("Function executed")
+    
+printit()
