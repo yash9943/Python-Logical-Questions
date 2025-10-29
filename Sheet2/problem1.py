370cf47c6dfd33420135a9d6890f5ee48e709a50
yash9943 <yr14112004@gmail.com>
2025-10-29 14:58:12 +0530

    Sheet2 Updated Ans

--- Sheet2/problem1.py
+++ Sheet2/problem1.py
@@ -7,3 +7,8 @@ string = "I love Python"
 
 final = string[7:13] +" "+string[2:6] +" "+ string[0]
 print(final)
+
+# word = string.split()
+# reversed_words = word[::-1]
+# final = " ".join(reversed_words)
+# print(final)
